67b5412883e7dac747359b11c9cc0996b7a7a540
Schampen <hampus.mannelev.ga.ntig.se>
2020-09-25 13:21:22 +0200

    episode 13 section 3 klar

--- app/Models/assignment.php
+++ app/Models/assignment.php
@@ -5,7 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Project extends Model
+class assignment extends Model
 {
     use HasFactory;
+
+    public function complete() 
+    {
+        $this->completed = true;
+        $this->save();
+    }
 }
