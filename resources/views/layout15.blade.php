29daf5e3a7a96150da10422f98ff6edf7723ab1b
Schampen <hampus.mannelev.ga.ntig.se>
2020-09-28 12:42:14 +0200

    episode 17 mix and webpack

--- resources/views/layout15.blade.php
+++ resources/views/layout15.blade.php
@@ -41,4 +41,9 @@ Released   : 20140225
 </div>
     @yield ('content')
 </body>
+<div id="copyright" class="container">
+    <p>&copy; Untitled. All rights reserved. | Photos by <a href="http://fotogrph.com/">Fotogrph</a> | Design by <a href="http://templated.co" rel="nofollow">TEMPLATED</a>.</p>
+</div>
+
+<script href="/js/app.js"></script>
 </html>
