dab5c5361311d4e9f5feb6a89cfceac3e25b54e2
Schampen <hampus.mannelev.ga.ntig.se>
2020-10-05 14:39:52 +0200

    episode 31 linking tables

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -20,4 +20,9 @@ class Article extends Model
     {
         return $this->belongsTo(User::Class, 'user_id');
     }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
 }
